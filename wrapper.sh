8f76cf1382eea54656399235df7f5424741f0199
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-08-29 13:15:39 +0300

    Set HOME for more complex sandboxing policies

--- wrapper.sh
+++ wrapper.sh
@@ -4,4 +4,4 @@ this=$(python -c "import os, sys; print(os.path.realpath(sys.argv[1]))" "$0")
 root=$(dirname "$this")
 root=$(dirname "$root")
 
-sandbox-exec -D root="$root" -f "$root/etc/profile.sb" env DYLD_ROOT_PATH="$root" "$root@TARGET@" "$@"
+sandbox-exec -D HOME="$HOME" -D root="$root" -f "$root/etc/profile.sb" env DYLD_ROOT_PATH="$root" "$root@TARGET@" "$@"
