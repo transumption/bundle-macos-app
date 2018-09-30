8fb4afd49d61f05b6cc41fee753cd0874e798b47
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-30 16:39:44 +0000

    Bump vendor, use standard macOS app layout, work around shebangs

--- default.nix
+++ default.nix
@@ -8,8 +8,8 @@ let
     name = "nix-vendor";
 
     src = fetchGit {
-      url = "https://github.com/serokell/nix-vendor";
-      rev = "a80cd5a378438e28d0760292c212b1410d408089";
+      url = https://github.com/serokell/nix-vendor;
+      rev = "1af6bb5ca45b4021702647872760d32a8199bd3a";
     };
 
     nativeBuildInputs = [ meson ninja ];
@@ -18,7 +18,10 @@ let
   stage1 = writeScript "stage-1" ''
     #!/bin/sh
 
-    ${stdenv.lib.optionalString withOpen "open"} "$(dirname "$0")${stage2}"
+    root=$(dirname "$0")
+    root=$(dirname "$root")/Resources
+
+    ${stdenv.lib.optionalString withOpen "open"} "$root${stage2}"
   '';
 
   stage2 = writeScript "stage-2" ''
@@ -29,7 +32,31 @@ let
     root=$(dirname "$root")
     root=$(dirname "$root")
 
-    env DYLD_INSERT_LIBRARIES="$root${nix-vendor}/lib/libnixvendor.dylib" DYLD_ROOT_PATH="$root" "$root${target}"
+    export DYLD_INSERT_LIBRARIES="$root${nix-vendor}/lib/libnixvendor.dylib"
+    export DYLD_ROOT_PATH="$root"
+
+    exec "$root${stdenv.shell}" "$root${target}"
+  '';
+
+  plist = ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>CFBundleGetInfoString</key>
+      <string>${name}</string>
+      <key>CFBundleExecutable</key>
+      <string>${name}</string>
+      <key>CFBundleIdentifier</key>
+      <string>org.nixos</string>
+      <key>CFBundleName</key>
+      <string>${name}</string>
+      <key>CFBundleInfoDictionaryVersion</key>
+      <string>6.0</string>
+      <key>CFBundlePackageType</key>
+      <string>APPL</string>
+    </dict>
+  </plist>
   '';
 in
 
@@ -37,14 +64,16 @@ stdenv.mkDerivation {
   inherit name;
 
   buildCommand = ''
-    mkdir -p "$out/Applications/${name}.app" && cd $_
+    mkdir -p "$out/Applications/${name}.app/Contents" && cd $_
+    mkdir -p MacOS Resources
+
+    install ${stage1} "MacOS/${name}"
 
     for path in $(< ${closure}/store-paths); do
-      cp --parents -r $path .
+      cp --parents -r $path Resources
     done
 
-    ${stdenv.lib.optionalString (icon != null) "cp ${icon} AppIcon.icns"}
-    install ${stage1} "${name}"
+    cp ${icon} "Resources/${name}.icns"
   '';
 
   meta = with stdenv.lib; {
